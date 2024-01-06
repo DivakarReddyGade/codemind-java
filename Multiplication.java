13fab53f587f183d2e3f7f119fac2a2f169ac9e8
DivakarReddyGade <119839595+DivakarReddyGade@users.noreply.github.com>
2024-01-06 09:54:31 +0530

    import java.util.Scanner; public class File {     public static void main(String[] divakar)     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         int c=a*b;         System.out.println(c);     } }

---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class File
+{
+    public static void main(String[] divakar)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=a*b;
+        System.out.println(c);
+    }
+}
