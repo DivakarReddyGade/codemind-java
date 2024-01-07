4dcb42cea99ccd88501287fb64540f9be0b20103
DivakarReddyGade <119839595+DivakarReddyGade@users.noreply.github.com>
2024-01-07 23:42:19 +0530

    import java.util.Scanner; public class File {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         char c=sc.next().charAt(0);         int a=c;         System.out.printf("%d",a);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class File
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        char c=sc.next().charAt(0);
+        int a=c;
+        System.out.printf("%d",a);
+    }
+}
